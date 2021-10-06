a4c3f1802feec4af56118f2cea0715fa909cead9
ah-james <11ahj11@gmail.com>
2021-10-06 17:46:02 -0400

    Moving DollarButton Logic to Home Container

--- src/components/DollarButton.js
+++ src/components/DollarButton.js
@@ -1,16 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 const DollarButton = props => {
-    const [count, setCount] = useState(0)
-
-    const imageClick = () => {
-      setCount(count + 1)
-    }
 
     return (
         <div>
-            <p>You have {count} dollars in the bank</p>
-            <img src={'https://upload.wikimedia.org/wikipedia/commons/2/23/US_one_dollar_bill%2C_obverse%2C_series_2009.jpg'} alt="logo" onClick={() => imageClick()} />
+            <img src={'https://upload.wikimedia.org/wikipedia/commons/2/23/US_one_dollar_bill%2C_obverse%2C_series_2009.jpg'} alt="logo" onClick={() => props.imageClick()} />
         </div>
     )
 }
