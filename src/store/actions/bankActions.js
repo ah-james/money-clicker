6d5f586074467b6903626fb2c49cec880966abd0
ah-james <11ahj11@gmail.com>
2021-10-12 17:08:31 -0400

    Increase Bank Action

---
+++ src/store/actions/bankActions.js
@@ -0,0 +1,20 @@
+export const increaseBank = (bank) => {
+    return async dispatch => {
+        try {
+            let response = await fetch('https://money-clicker-195e4-default-rtdb.firebaseio.com', {
+                method: 'PATCH',
+                body: JSON.stringify({bank: bank + 1}),
+                headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
+            })
+
+            if (!response.ok) {
+                throw new Error('something went wrong!')
+            }
+
+            let json = await response.json()
+            dispatch({type: 'INCREMENT', payload: json['data']})
+        } catch (error) {
+            throw error
+        }
+    }
+}
