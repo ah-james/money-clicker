6c1dc0695c0939b5cd322c31d75230f61d23d0c5
ah-james <11ahj11@gmail.com>
2021-10-22 16:06:16 -0400

    Adding TellerContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import HomeContainer from './containers/HomeContainer';
+import TellerContainer from './containers/TellerContainer'
 
 const App = props => {
   return (
     <div className="App">
       <header className="App-header">
         <HomeContainer />
+        <TellerContainer />
       </header>
     </div>
   );
---
+++ src/containers/TellerContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TellerContainer = props => {
+
+    return(
+        <div>
+            <p>These are your Tellers:</p>
+        </div>
+    )
+}
+
+export default TellerContainer
