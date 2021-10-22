56ae448d4ace575ff492c0c3aac2de8c745d9439
ah-james <11ahj11@gmail.com>
2021-10-22 16:34:20 -0400

    Attaching TellerContainer to Teller Store

--- src/containers/TellerContainer.js
+++ src/containers/TellerContainer.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import * as tellerActions from '../store/actions/tellerActions'
+import TellerButton from '../components/TellerButton'
 
 const TellerContainer = props => {
+
+    const tellers = useSelector(state => state.tellers)
+    const dispatch = useDispatch()
+
+    const loadPage = useCallback(async () => {
+        try {
+            await dispatch(tellerActions.fetchTellers())
+        } catch (error) {
+            console.log(error.message)
+        }
+    }, [dispatch])
+
+    useEffect(() => {
+        loadPage()
+    }, [loadPage])
 
     return(
         <div>
             <p>These are your Tellers:</p>
+            {tellers.map((teller) => {
+                return <TellerButton name={teller} />
+            })}
         </div>
     )
 }
